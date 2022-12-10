1f2777be7e858dd6af67fb7bfd5312f0bcc80868
+ <+>
2022-12-10 12:07:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(driverNames,sought){
+    return driverNames.filter(
+        (possibleMatch)=>possibleMatch.toLowerCase()===sought.toLowerCase()
+
+    );
+}
+function fuzzyMatch(driverNames,testString){
+    return driverNames.filter(
+        (possibleMatch)=>possibleMatch.toLowerCase().indexOf(testString.toLowerCase())===0
+    );
+}
+function matchName(driverNames,soughtNames){
+    return driverNames.filter((record)=>record.name===soughtNames);
+}
